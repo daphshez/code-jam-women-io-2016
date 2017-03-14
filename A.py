114a8c60ef723941be1126394d2d950be081541f
Daphna Shezaf <daphna@tootle.co.uk>
2017-03-14 14:43:44 +0000

    A passes test

--- A.py
+++ A.py
@@ -1,30 +1,32 @@
 """
 Problem
 
-Cody, the owner of the legendary Cody's Jams store, is planning a huge jam sale. To make things simple, he has decided to sell every item in his store at 
-a 25% discount — that is, each item's sale price is exactly 75% of its regular price. Since all of his regular prices happened to be integers divisible 
-by four, his sale prices are conveniently also all integers.
+Cody, the owner of the legendary Cody's Jams store, is planning a huge jam sale. To make things simple, he has decided
+to sell every item in his store at a 25% discount — that is, each item's sale price is exactly 75% of its regular price.
+Since all of his regular prices happened to be integers divisible by four, his sale prices are conveniently also all
+integers.
 
-To prepare for the sale, he placed an order to print new labels for all of his items at their sale prices. He also placed an order to print new labels 
-for all of his items at their regular prices, to use once the sale is over.
+To prepare for the sale, he placed an order to print new labels for all of his items at their sale prices. He also
+placed an order to print new labels for all of his items at their regular prices, to use once the sale is over.
 
-Cody just came back from picking up his order. Unfortunately, the printer gave him both orders in one combined stack, sorted by price. Both the sale 
-price and the regular price label for each item are present somewhere in the stack. However, both types of labels look the same, and since he does 
-not remember the price of every item, he is not sure which labels are the sale price labels. Can you figure that out?
+Cody just came back from picking up his order. Unfortunately, the printer gave him both orders in one combined stack,
+sorted by price. Both the sale price and the regular price label for each item are present somewhere in the stack.
+However, both types of labels look the same, and since he does not remember the price of every item, he is not sure
+which labels are the sale price labels. Can you figure that out?
 
-For instance, if the regular prices were 20, 80, and 100, the sale prices would be 15, 60, and 75, and the printer's stack would consist of the 
-labels 15, 20, 60, 75, 80, and 100.
+For instance, if the regular prices were 20, 80, and 100, the sale prices would be 15, 60, and 75, and the printer's
+stack would consist of the labels 15, 20, 60, 75, 80, and 100.
 
 Input
 
-The first line of the input gives the number of test cases, T. T test cases follow. Each test case consists of two lines. The first line contains a 
-single integer N, the number of items in Cody's store. The second line contains 2N integers P1, P2, ..., P2N in non-decreasing order by the price 
-printed on each label given by the printer.
+The first line of the input gives the number of test cases, T. T test cases follow. Each test case consists of two
+lines. The first line contains a single integer N, the number of items in Cody's store. The second line contains 2N
+integers P1, P2, ..., P2N in non-decreasing order by the price printed on each label given by the printer.
 
 Output
 
-For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is a list of N integers: the 
-labels containing sale prices, in non-decreasing order.
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is a
+list of N integers: the labels containing sale prices, in non-decreasing order.
 
 Limits
 
@@ -53,9 +55,8 @@ Case #2: 9 9 12 15
 
 """
 
-from collections import Counter
-
 from CodeJam import CodeJamProblem
+from collections import Counter
 
 
 class A(CodeJamProblem):
@@ -63,11 +64,20 @@ class A(CodeJamProblem):
         super().__init__('A')
 
     def generate_test_cases(self, input_file):
-        pass
+        with open(input_file) as f:
+            for _ in range(int(next(f).strip())):
+                next(f)
+                yield [int(x) for x in next(f).strip().split(' ')]
 
     def solve(self, t):
-        pass
-
+        c = Counter(t)
+        r = []
+        for x in t:
+            if c[x] > 0:
+                r.append(x)
+                c[x] -= 1
+                c[x // 3 * 4] -= 1
+        return " ".join(str(x) for x in r)
 
 
 a = A()
